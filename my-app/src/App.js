a479fbef1dc609f88512487d5d3e254a6ce1f212
engmutabazi100 <engmutabazi2020@gmail.com>
2020-09-28 07:38:29 -0700

    initialize git

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MaterialTableDemo from './Table';
+import Post from './Post';
+
+
+export default function App(props) {
+  
+  return (
+    <div>
+<MaterialTableDemo/>
+{/* <Post/> */}
+    </div>
+  )  
+}
---
+++ my-app/src/exercise.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import TextField from '@material-ui/core/TextField';
+
+export default function exercise() {
+    return (
+
+        
+// render
+
+<TextField
+  label="With normal TextField"
+  InputProps={{
+    endAdornment: (
+      <InputAdornment>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </InputAdornment>
+    )
+  }}
+/>
+       
+    )
+}
